1ed6cba823eb90695bab378db78c2dd2d57781f5
Shashank <shashankb150@gmail.com>
2026-07-15 23:52:46 +0530

    create jobAPI Request

---
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -0,0 +1,52 @@
+package com.api.tests;
+
+import org.testng.annotations.Test;
+
+import com.api.constants.Roles;
+import com.api.pojo.CreateJobPayload;
+import com.api.pojo.Customer;
+import com.api.pojo.Customer_Address;
+import com.api.pojo.Customer_product;
+import com.api.pojo.Problems;
+import com.api.utils.AuthTokenProvider;
+import com.api.utils.ConfigManager;
+import com.api.utils.SpecUtil;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class CreateJobAPITest {
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	public void createJobAPITest() {
+		//creating the CreateJobPayload Obect
+		Problems problems = new Problems(1, "Battery Issue");
+		Problems[] problemsArray = new Problems[1];
+		problemsArray[0]=problems;
+		
+		Customer_product customer_Product = new Customer_product("2025-04-06T18:30:00.000Z", "12897387233126","12897387233126" , "12897387233126", "2025-04-06T18:30:00.000Z", 1, 1);
+		Customer_Address customer_address = new Customer_Address("32 UPSC House", "H 1705", "Delhi", "", "Delhi", "411057", "INDIA", "Delhi");
+		Customer customer = new Customer("Ron", "Messi", "9900220000", "", "bond@7mail.com", "");
+		CreateJobPayload createJobPayLoad = new CreateJobPayload(0, 2, 1, 1, customer, customer_address, customer_Product, problemsArray);
+		
+		
+	RestAssured.given().
+	spec(SpecUtil.requestSpecWithAuth(Roles.FD, createJobPayLoad))
+		.log().all()
+		.when().
+		post("/job/create").
+		then().
+		spec(SpecUtil.responseSpec());
+		
+		
+		
+		
+	}
+
+}
